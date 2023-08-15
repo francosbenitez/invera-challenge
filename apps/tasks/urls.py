6eede805fcb3462f630004a3fe15111415973206
Franco <francosbenitez@gmail.com>
2023-08-15 00:40:39 -0300

    fix: tasks url

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -8,8 +8,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path("tasks/", TaskListAPIView.as_view(), name="task-list"),
-    path("tasks/create/", TaskCreateAPIView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/", TaskUpdateAPIView.as_view(), name="task-update"),
-    path("tasks/<int:pk>/delete/", TaskDestroyAPIView.as_view(), name="task-delete"),
+    path("", TaskListAPIView.as_view(), name="task-list"),
+    path("create/", TaskCreateAPIView.as_view(), name="task-create"),
+    path("<int:pk>/", TaskUpdateAPIView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDestroyAPIView.as_view(), name="task-delete"),
 ]
