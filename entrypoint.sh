9f22da8d2f5debd7fbc3044ab77a39b4a4c77204
Franco <francosbenitez@gmail.com>
2023-08-15 21:55:49 -0300

    refactor: entrypoint v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
+# Run necessary Django commands
+echo "Running Django migrations..."
 python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
+# Set up port configuration
 DEFAULT_PORT=8000
 PORT=${PORT:-$DEFAULT_PORT}
 
+# Start Gunicorn
+echo "Starting Gunicorn..."
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+
+# Check if Gunicorn started successfully
+if [ $? -ne 0 ]; then
+    echo "Failed to start Gunicorn."
+    exit 1
+fi
+
+echo "Django application is running on port $PORT."
