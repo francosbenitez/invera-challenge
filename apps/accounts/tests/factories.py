41f251fc5c50188c2dd77d35dc68a5fbda78ae87
Franco <francosbenitez@gmail.com>
2023-08-17 00:09:20 -0300

    feat: add userfactory

---
+++ apps/accounts/tests/factories.py
@@ -0,0 +1,31 @@
+import factory
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from factory.django import DjangoModelFactory
+
+from apps.tasks.models import Task
+
+User = get_user_model()
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    email = factory.Sequence(lambda n: f"user{n}@example.com")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_active = True
+    is_admin = False
+    is_staff = False
+    created_at = factory.LazyFunction(timezone.now)
+    updated_at = factory.LazyFunction(timezone.now)
+
+    @factory.post_generation
+    def tasks(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for task in extracted:
+                self.tasks.add(task)
