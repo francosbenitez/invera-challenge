1f9833518011723e356ac06a16bedb2b72c4c1de
Franco <francosbenitez@gmail.com>
2023-08-17 22:20:18 -0300

    chore: logger in tests

--- apps/accounts/tests/test_views.py
+++ apps/accounts/tests/test_views.py
@@ -7,35 +7,20 @@ logger = getLogger(__name__)
 
 @pytest.mark.django_db
 def test_successful_user_registration(api_client, registration_url, user_data):
-    """
-    Test successful user registration.
-    """
-
     response = api_client.post(registration_url, user_data)
     assert response.status_code == 201
 
 
 @pytest.mark.django_db
 def test_successful_user_login(api_client, login_url, valid_user):
-    """
-    Test successful user login.
-    """
-
     response = api_client.post(
         login_url, {"email": valid_user.email, "password": "holaquetal"}
     )
-
-    logger.info(response.json())
-
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_failed_login_invalid_credentials(api_client, login_url, invalid_user):
-    """
-    Test failed user login due to invalid credentials.
-    """
-
     response = api_client.post(login_url, invalid_user)
     assert response.status_code == 400
     assert response.json() == {
--- apps/tasks/tests/test_views.py
+++ apps/tasks/tests/test_views.py
@@ -1,16 +1,16 @@
+from logging import getLogger
+
 import pytest
 from django.urls import reverse
 from rest_framework import status
 
 from apps.tasks.models import Task
 
+logger = getLogger(__name__)
+
 
 @pytest.mark.django_db
 def test_create_task(authenticated_api_client, task_data):
-    """
-    Test to verify that a task can be created via POST request to the task-create endpoint.
-    """
-
     url = reverse("task-create")
     response = authenticated_api_client.post(url, data=task_data)
     assert response.status_code == status.HTTP_201_CREATED
@@ -19,10 +19,6 @@ def test_create_task(authenticated_api_client, task_data):
 
 @pytest.mark.django_db
 def test_list_tasks(authenticated_api_client):
-    """
-    Test to verify that a task can be listed via GET request to the task-list endpoint.
-    """
-
     url = reverse("task-list")
     response = authenticated_api_client.get(url)
     assert response.status_code == status.HTTP_200_OK
@@ -30,10 +26,6 @@ def test_list_tasks(authenticated_api_client):
 
 @pytest.mark.django_db
 def test_update_task(authenticated_api_client, task, task_update_data):
-    """
-    Test to verify that a task can be updated via PATCH request to the task-update endpoint.
-    """
-
     url = reverse("task-update", kwargs={"pk": task.id})
     response = authenticated_api_client.patch(url, data=task_update_data)
     assert response.status_code == status.HTTP_200_OK
@@ -44,10 +36,6 @@ def test_update_task(authenticated_api_client, task, task_update_data):
 
 @pytest.mark.django_db
 def test_delete_task(authenticated_api_client, task):
-    """
-    Test to verify that a task can be deleted via DELETE request to the task-delete endpoint.
-    """
-
     url = reverse("task-delete", kwargs={"pk": task.id})
     response = authenticated_api_client.delete(url)
     assert response.status_code == status.HTTP_204_NO_CONTENT
