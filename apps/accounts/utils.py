3a5b6417087d6952465e1547eab0ff298560fb15
Franco <francosbenitez@gmail.com>
2023-08-19 00:45:22 -0300

    refactor: DRY assignment & removal functionalities

---
+++ apps/accounts/utils.py
@@ -0,0 +1,75 @@
+from logging import getLogger
+
+from rest_framework import status
+from rest_framework.response import Response
+
+from apps.tasks.models import Task
+
+logger = getLogger(__name__)
+
+
+def handle_task_action(request, action):
+    """
+    Handles task assignment and removal.
+    """
+
+    word = "assigned" if action == "assignment" else "removed"
+
+    if not request.data.get("task_id"):
+        logger.error("No task id provided.")
+        return Response(
+            {"detail": "No task id provided.", "code": "no_task_id_provided"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    task_ids = request.data.getlist("task_id")
+
+    if not task_ids:
+        logger.error("No task ids provided.")
+        return Response(
+            {"detail": "No task ids provided.", "code": "no_task_ids_provided"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    for task_id in task_ids:
+        try:
+            task = Task.objects.get(id=task_id)
+        except Task.DoesNotExist:
+            logger.error(f"Task with id {task_id} does not exist.")
+            return Response(
+                {"detail": "Task does not exist.", "code": "task_does_not_exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if action == "removal":
+            if task not in request.user.tasks.all():
+                logger.error(f"Task with id {task_id} is not {word} to the user.")
+                return Response(
+                    {
+                        "detail": f"Task is not {word} to the user.",
+                        "code": f"task_not_{word}_to_user",
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            request.user.tasks.remove(task)
+        elif action == "assignment":
+            if task in request.user.tasks.all():
+                logger.error(f"Task with id {task_id} is already {word} to the user.")
+                return Response(
+                    {
+                        "detail": f"Task is already {word} to the user.",
+                        "code": f"task_already_{word}_to_user",
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            request.user.tasks.add(task)
+
+    logger.info(f"Task with ids {task_ids} have been {word} from the user.")
+
+    return Response(
+        {
+            "detail": f"Tasks {word} successfully.",
+            "code": f"tasks_{word}_successfully",
+        },
+        status=status.HTTP_200_OK,
+    )
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,11 @@
 from logging import getLogger
 
-from rest_framework import status
 from rest_framework.generics import ListAPIView
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.tasks.models import Task
 from utils.mixins import TaskMixin
+
+from .utils import handle_task_action
 
 logger = getLogger(__name__)
 
@@ -26,45 +25,7 @@ class UserTaskAssignmentAPIView(TaskMixin, APIView):
     """
 
     def post(self, request):
-        if not request.data.get("task_id"):
-            logger.error("No task id provided.")
-            return Response(
-                {"detail": "No task id provided.", "code": "no_task_id_provided"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        task_ids = request.data.getlist("task_id")
-
-        for task_id in task_ids:
-            try:
-                task = Task.objects.get(id=task_id)
-            except Task.DoesNotExist:
-                logger.error(f"Task with id {task_id} does not exist.")
-                return Response(
-                    {"detail": "Task does not exist.", "code": "task_does_not_exist"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            if task in request.user.tasks.all():
-                logger.error(f"Task with id {task_id} is already assigned to the user.")
-                return Response(
-                    {
-                        "detail": "Task is already assigned to the user.",
-                        "code": "task_already_assigned_to_user",
-                    },
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            request.user.tasks.add(task)
-
-        logger.info(f"Tasks with ids {task_ids} have been assigned to the user.")
-        return Response(
-            {
-                "detail": "Tasks assigned successfully.",
-                "code": "tasks_assigned_successfully",
-            },
-            status=status.HTTP_200_OK,
-        )
+        return handle_task_action(request, "assignment")
 
 
 class UserTaskRemovalAPIView(APIView):
@@ -73,49 +34,4 @@ class UserTaskRemovalAPIView(APIView):
     """
 
     def delete(self, request):
-        if not request.data.get("task_id"):
-            logger.error("No task id provided.")
-            return Response(
-                {"detail": "No task id provided.", "code": "no_task_id_provided"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        task_ids = request.data.getlist("task_id")
-
-        if not task_ids:
-            logger.error("No task ids provided.")
-            return Response(
-                {"detail": "No task ids provided.", "code": "no_task_ids_provided"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        for task_id in task_ids:
-            try:
-                task = Task.objects.get(id=task_id)
-            except Task.DoesNotExist:
-                logger.error(f"Task with id {task_id} does not exist.")
-                return Response(
-                    {"detail": "Task does not exist.", "code": "task_does_not_exist"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            if task not in request.user.tasks.all():
-                logger.error(f"Task with id {task_id} is not assigned to the user.")
-                return Response(
-                    {
-                        "detail": "Task is not assigned to the user.",
-                        "code": "task_not_assigned_to_user",
-                    },
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            request.user.tasks.remove(task)
-
-        logger.info(f"Task with ids {task_ids} have been removed from the user.")
-        return Response(
-            {
-                "detail": "Tasks removed successfully.",
-                "code": "tasks_removed_successfully",
-            },
-            status=status.HTTP_200_OK,
-        )
+        return handle_task_action(request, "removal")
