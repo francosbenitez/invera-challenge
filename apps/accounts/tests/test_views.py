4446030429aad32327595fcf16c54d7cd2bb54f9
Franco <francosbenitez@gmail.com>
2023-08-19 00:13:34 -0300

    refactor: tests naming

--- apps/accounts/tests/test_views.py
+++ apps/accounts/tests/test_views.py
@@ -10,7 +10,7 @@ logger = getLogger(__name__)
 @pytest.mark.django_db
 def test_successful_user_registration(api_client, registration_url, user_data):
     response = api_client.post(registration_url, user_data)
-    assert response.status_code == 201
+    assert response.status_code == status.HTTP_201_CREATED
 
 
 @pytest.mark.django_db
@@ -18,20 +18,20 @@ def test_successful_user_login(api_client, login_url, valid_user):
     response = api_client.post(
         login_url, {"email": valid_user.email, "password": "holaquetal"}
     )
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.django_db
-def test_failed_login_invalid_credentials(api_client, login_url, invalid_user):
+def test_failed_login_with_invalid_credentials(api_client, login_url, invalid_user):
     response = api_client.post(login_url, invalid_user)
-    assert response.status_code == 400
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {
         "non_field_errors": ["Unable to log in with provided credentials."]
     }
 
 
 @pytest.mark.django_db
-def test_user_task_list_view(authenticated_api_client, assigned_task):
+def test_user_task_list(authenticated_api_client, assigned_task):
     url = reverse("user-task-list")
     response = authenticated_api_client.get(url)
     assert response.status_code == status.HTTP_200_OK
@@ -39,7 +39,7 @@ def test_user_task_list_view(authenticated_api_client, assigned_task):
 
 
 @pytest.mark.django_db
-def test_user_task_assignment_view(authenticated_api_client, task):
+def test_user_task_assignment(authenticated_api_client, task):
     url = reverse("user-task-assignment")
     data = {"task_id": task.id}
     response = authenticated_api_client.post(url, data=data)
@@ -51,7 +51,7 @@ def test_user_task_assignment_view(authenticated_api_client, task):
 
 
 @pytest.mark.django_db
-def test_user_task_removal_view(authenticated_api_client, assigned_task):
+def test_user_task_removal(authenticated_api_client, assigned_task):
     url = reverse("user-task-removal")
     data = {"task_id": assigned_task.id}
     response = authenticated_api_client.delete(url, data=data)
