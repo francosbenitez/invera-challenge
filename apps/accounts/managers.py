3a8a86ef22ffd4486b48ebed589407c3e30a733b
Franco <francosbenitez@gmail.com>
2023-08-20 23:07:56 -0300

    chore: some general cleaning up v2

--- apps/accounts/managers.py
+++ apps/accounts/managers.py
@@ -21,7 +21,6 @@ class UserManager(BaseUserManager):
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-
         user.set_password(password)
         user.save()
 
